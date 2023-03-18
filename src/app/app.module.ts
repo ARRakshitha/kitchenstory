0dcc00d513726a688868e7bb67523ad8d6bd0a32
ARRakshitha <rakshithaar878@gmail.com>
2023-03-18 19:34:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdditemComponent } from './additem/additem.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { HomeComponent } from './home/home.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { UpdateItemComponent } from './update-item/update-item.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdditemComponent,
+    AdminLoginComponent,
+    AllOrdersComponent,
+    HomeComponent,
+    PlaceOrderComponent,
+    UpdateItemComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
